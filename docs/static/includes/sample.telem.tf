c4c157ae2dfbc025b09d9e5a99bcaa3930fe3edc
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-08-29 14:19:56 +0100

    update contrib for tf

--- docs/static/includes/sample.telem.tf
+++ docs/static/includes/sample.telem.tf
@@ -19,17 +19,28 @@ TEMPLATE
   # This is the unique id for the module that is supplied by the AVM team.
   telem_puid = "00000000"
 
+  # TODO: change this to the name of the module. See https://azure.github.io/Azure-Verified-Modules/specs/shared/#id-sfr3---category-telemetry---deploymentusage-telemetry
+  module_name = "CHANGEME"
+
   # This constructs the ARM deployment name that is used for the telemetry.
   # We shouldn't ever hit the 64 character limit but use substr just in case.
   telem_arm_deployment_name = substr(
     format(
-      "pid-%s_%s",
+      "%s.%s.%s.%s",
       local.telem_puid,
-      local.module_version,
+      substr(local.module_name, 0, 30),
+      replace(local.module_version, ".", "-"),
+      local.telem_random_hex
     ),
     0,
     64
   )
+  telem_random_hex = can(random_id.telem[0].hex) ? random_id.telem[0].hex : ""
+}
+
+resource "random_id" "telemetry" {
+  count       = local.enable_telemetry ? 1 : 0
+  byte_length = 4
 }
 
 # This is the module telemetry deployment that is only created if telemetry is enabled.
@@ -38,6 +49,7 @@ resource "azurerm_resource_group_template_deployment" "telemetry" {
   count               = var.enable_telemetry ? 1 : 0
   name                = local.telem_arm_deployment_name
   resource_group_name = var.resource_group_name
+  deployment_mode     = "Incremental"
   location            = var.location
   template_content    = local.telem_arm_template_content
 }
