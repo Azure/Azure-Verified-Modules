edaba13120e519d6c33f3506ddbf1f7702e1b58a
Jack Tracey <jack@jacktracey.co.uk>
2023-09-19 12:05:02 +0100

    update locks tf

--- docs/static/includes/interfaces/int.locks.schema.tf
+++ docs/static/includes/interfaces/int.locks.schema.tf
@@ -1,12 +1,21 @@
 variable "lock" {
   type = object({
-    name = string
-    type = optional(string, "CanNotDelete")
+    name = optional(string, null)
+    kind = optional(string, "None")
   })
   default = {}
 
   validation {
-    condition     = contains(["CanNotDelete", "ReadOnly"], var.lock.type)
-    error_message = "Lock type must be one of: CanNotDelete, ReadOnly."
+    condition     = contains(["CanNotDelete", "ReadOnly", "None"], var.lock.type)
+    error_message = "Lock type must be one of: CanNotDelete, ReadOnly, None."
   }
+}
+
+# Example declaration of the resource
+
+resource "azurerm_management_lock" "this" {
+  count      = var.lock.kind != "None" ? 1 : 0
+  name       = coalesce(var.lock.name, "lock-${var.name}")
+  scope      = # Your resource ID here
+  lock_level = var.lock.kind
 }
