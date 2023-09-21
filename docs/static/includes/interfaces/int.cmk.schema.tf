6aab09785e73217cd1bff4795e7f8b31e2c75647
Alexander Sehr <ASehr@hotmail.de>
2023-09-21 15:03:53 +0100

    Made UserAssignedIdentities optional in CMK schema (#72)
    Co-authored-by: Jack Tracey <jack@jacktracey.co.uk>

--- docs/static/includes/interfaces/int.cmk.schema.tf
+++ docs/static/includes/interfaces/int.cmk.schema.tf
@@ -3,7 +3,7 @@ variable "customer_managed_key" {
     key_vault_resource_id              = string
     key_name                           = string
     key_version                        = optional(string, null)
-    user_assigned_identity_resource_id = string
+    user_assigned_identity_resource_id = optional(string, null)
   })
   default = {}
 }
