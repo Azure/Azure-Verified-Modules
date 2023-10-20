a840cc115f5baaa4308814e1e158c7b9ca18a5fd
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-10-20 08:27:18 +0100

    fix: update diag settings

--- docs/static/includes/interfaces/int.diag.input.tf
+++ docs/static/includes/interfaces/int.diag.input.tf
@@ -1,7 +1,7 @@
 diagnostic_settings = {
   diag_setting_1 = {
     name                                     = "diagSetting1"
-    log_categories_and_groups                = ["allLogs"]
+    log_groups                               = ["allLogs"]
     metric_categories                        = ["AllMetrics"]
     log_analytics_destination_type           = "Dedicated"
     workspace_resource_id                    = "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/{workspaceName}"
