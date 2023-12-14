d1f8a596ea92727e91329aff73e7d99ecdff5faa
Marius Storhaug <marstor@hotmail.com>
2023-12-14 13:12:06 +0100

    Suggested changes to lock interface for TF

--- docs/static/includes/interfaces/int.locks.schema.tf
+++ docs/static/includes/interfaces/int.locks.schema.tf
@@ -1,21 +1,21 @@
 variable "lock" {
   type = object({
     name = optional(string, null)
-    kind = optional(string, "None")
+    kind = string
   })
-  description = "The lock level to apply to the Key Vault. Possible values are `None`, `CanNotDelete`, and `ReadOnly`."
-  default     = {}
-  nullable    = false
+  description = "The lock level to apply to the resource. Possible values are `CanNotDelete` and `ReadOnly`."
+  default     = null
   validation {
-    condition     = contains(["CanNotDelete", "ReadOnly", "None"], var.lock.kind)
-    error_message = "The lock level must be one of: 'None', 'CanNotDelete', or 'ReadOnly'."
+    condition     = contains(["CanNotDelete", "ReadOnly"], var.lock.kind)
+    error_message = "The lock level must be one of: 'CanNotDelete', 'ReadOnly'."
   }
 }
-
+  
 # Example resource implementation
 resource "azurerm_management_lock" "this" {
-  count      = var.lock.kind != "None" ? 1 : 0
+  count      = var.lock != null ? 1 : 0
   name       = coalesce(var.lock.name, "lock-${var.name}")
   scope      = azurerm_MY_RESOURCE.this.id
   lock_level = var.lock.kind
+  notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete resource or child resources." : "Cannot delete or modify the resource or child resources."
 }
