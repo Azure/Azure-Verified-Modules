31e04ec75b5134531d532379b4c458f569c5dc52
Marius Storhaug <marstor@hotmail.com>
2023-12-15 20:52:58 +0100

    fix

--- docs/static/includes/interfaces/int.locks.schema.tf
+++ docs/static/includes/interfaces/int.locks.schema.tf
@@ -3,19 +3,24 @@ variable "lock" {
     name = optional(string, null)
     kind = string
   })
-  description = "The lock level to apply to the resource. Possible values are `CanNotDelete` and `ReadOnly`."
+  description = <<-EOT
+Applies a lock to the resource. The following properties can be specified:
+- name - (Optional) The name of the lock. Changing this forces a new resource to be created.
+- kind - (Required) The type of lock. Possible values are `CanNotDelete` and `ReadOnly`.
+EOT
   default     = null
   validation {
     condition     = var.lock == null || contains(["CanNotDelete", "ReadOnly"], try(var.lock.kind, ""))
-    error_message = "The lock level must be one of: 'CanNotDelete', or 'ReadOnly'."
+    error_message = "The lock level must be one of: 'CanNotDelete', 'ReadOnly'."
   }
 }
 
 # Example resource implementation
 resource "azurerm_management_lock" "this" {
   count      = var.lock != null ? 1 : 0
+
   name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_MY_RESOURCE.this.id
+  scope      = azurerm_kubernetes_cluster.main.id
   lock_level = var.lock.kind
   notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete resource or child resources." : "Cannot delete or modify the resource or child resources."
 }
