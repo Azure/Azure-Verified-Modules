d3757e288a4ad0575b3ade7718f313da37741087
Marius Storhaug <marstor@hotmail.com>
2024-01-04 13:14:02 +0100

    validated suggestion + feedback from Matt

--- docs/static/includes/interfaces/int.locks.schema.tf
+++ docs/static/includes/interfaces/int.locks.schema.tf
@@ -1,26 +1,31 @@
 variable "lock" {
   type = object({
-    name = optional(string, null)
     kind = string
+    name = optional(string, null)
   })
-  description = <<-EOT
-Applies a lock to the resource. The following properties can be specified:
-- name - (Optional) The name of the lock. Changing this forces a new resource to be created.
-- kind - (Required) The type of lock. Possible values are `CanNotDelete` and `ReadOnly`.
-EOT
-  default     = null
+  default     = {
+    kind = null
+  }
+  description = <<DESCRIPTION
+Controls the Resource Lock configuration on this resource. The following properties can be specified:
+
+- `kind` - (Required) The type of lock. Possible values are `null`, `CanNotDelete` and `ReadOnly`.
+- `name` - (Optional) The name of the lock. If not specified a name will be generated based on the `kind` value. Changing this forces a new resource to be created.
+DESCRIPTION
+  nullable    = false
+
   validation {
-    condition     = var.lock == null || contains(["CanNotDelete", "ReadOnly"], try(var.lock.kind, ""))
-    error_message = "The lock level must be one of: 'CanNotDelete', 'ReadOnly'."
+    condition     = var.lock.kind != null ? contains(["CanNotDelete", "ReadOnly"], var.lock.kind) : true
+    error_message = "The lock level must be one of: `null`, `CanNotDelete` and `ReadOnly`."
   }
 }
 
 # Example resource implementation
 resource "azurerm_management_lock" "this" {
-  count      = var.lock != null ? 1 : 0
+  count = var.lock.kind != null ? 1 : 0
 
+  lock_level = var.lock.kind
   name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
   scope      = azurerm_MY_RESOURCE.this.id
-  lock_level = var.lock.kind
   notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete resource or child resources." : "Cannot delete or modify the resource or child resources."
 }
