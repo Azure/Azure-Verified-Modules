cc7f7bfc64b159c2a16eec60981f68f1f2f37827
Marius Storhaug <marstor@hotmail.com>
2024-01-04 13:22:33 +0100

    Language fix in description

--- docs/static/includes/interfaces/int.locks.schema.tf
+++ docs/static/includes/interfaces/int.locks.schema.tf
@@ -9,14 +9,14 @@ variable "lock" {
   description = <<DESCRIPTION
 Controls the Resource Lock configuration on this resource. The following properties can be specified:
 
-- `kind` - (Required) The type of lock. Possible values are `null`, `CanNotDelete` and `ReadOnly`.
+- `kind` - (Required) The type of lock. Possible values are `null`, `CanNotDelete` or `ReadOnly`.
 - `name` - (Optional) The name of the lock. If not specified a name will be generated based on the `kind` value. Changing this forces a new resource to be created.
 DESCRIPTION
   nullable    = false
 
   validation {
     condition     = var.lock.kind != null ? contains(["CanNotDelete", "ReadOnly"], var.lock.kind) : true
-    error_message = "The lock level must be one of: `null`, `CanNotDelete` and `ReadOnly`."
+    error_message = "The lock level must be one of: `null`, `CanNotDelete` or `ReadOnly`."
   }
 }
 
