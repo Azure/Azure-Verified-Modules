b95c8b3a52f3f5800310cb911ed838f77d9550de
lonegunmanb <lonegunmanb@hotmail.com>
2024-02-08 03:22:10 +0000

    fix incorrect resource example

--- docs/static/includes/interfaces/int.rbac.schema.tf
+++ docs/static/includes/interfaces/int.rbac.schema.tf
@@ -28,8 +28,8 @@ DESCRIPTION
 resource "azurerm_role_assignment" "this" {
   for_each                               = var.role_assignments
   scope                                  = azurerm_MY_RESOURCE.this.id
-  role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
-  role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
+  role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
+  role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
   principal_id                           = each.value.principal_id
   condition                              = each.value.condition
   condition_version                      = each.value.condition_version
