784f30c3dc3fccf9e79c4d7110a2152f70d9e89e
Marius Storhaug <marstor@hotmail.com>
2024-02-22 22:47:12 +0100

    reset interface, not dynamic blocks...

--- docs/static/includes/interfaces/int.tags.schema.tf
+++ docs/static/includes/interfaces/int.tags.schema.tf
@@ -1,4 +1,7 @@
+# Having default empty map and `nullable = false` means that
+# any expressions within the module are simpler.
 variable "tags" {
-  type    = map(string)
-  default = null
+  type     = map(string)
+  default  = {}
+  nullable = false
 }
