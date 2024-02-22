9cd1e4290dba691019a0c3d76c09107478071ced
Marius Storhaug <marstor@hotmail.com>
2024-02-22 15:20:11 +0100

    indent in an example

--- docs/static/includes/sample.var_example.tf
+++ docs/static/includes/sample.var_example.tf
@@ -3,25 +3,25 @@ variable "my_complex_input" {
     param1 = string
     param2 = optional(number, null)
   }))
-  description = <<DESCRIPTION
-A complex input variable that is a map of objects.
-Each object has two attributes:
+  description = <<-DESCRIPTION
+    A complex input variable that is a map of objects.
+    Each object has two attributes:
 
-- `param1`: A required string parameter.
-- `param2`: (Optional) An optional number parameter.
+    - `param1`: A required string parameter.
+    - `param2`: (Optional) An optional number parameter.
 
-Example Input:
+    Example Input:
 
-```terraform
-my_complex_input = {
-  "object1" = {
-    param1 = "value1"
-    param2 = 2
-  }
-  "object2" = {
-    param1 = "value2"
-  }
-}
-```
-DESCRIPTION
+    ```terraform
+    my_complex_input = {
+      "object1" = {
+        param1 = "value1"
+        param2 = 2
+      }
+      "object2" = {
+        param1 = "value2"
+      }
+    }
+    ```
+  DESCRIPTION
 }
