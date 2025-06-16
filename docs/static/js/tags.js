0f65008f88e94c41f704447d6e48cdd802580c3f
René Hézser <rene@hezser.de>
2025-06-16 21:25:05 +0200

    add title attribute to tags

---
+++ docs/static/js/tags.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const tags = document.querySelectorAll('span.taxonomy-tags');
+
+  // load all tags, that should be enriched with a tooltip
+  var tagListUrl = paramsBase + '/js/taglist.json';
+  fetch(tagListUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const taglist = data || [];
+    // enrich tags with tooltip
+    tags.forEach(function(tag) {
+      try {
+        var tagName = tag.textContent.trim();
+        var tagEnrichment = taglist[tagName];
+        if (tagEnrichment !== undefined) {
+          tag.title = tagEnrichment.name;
+          if (tagEnrichment.description) {
+            tag.title += ' - ' + tagEnrichment.description;
+          }
+        }
+      } catch (error) {
+        console.error('Error getting current script URL:', error);
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error loading taglist.json:', error);
+  });
+});
