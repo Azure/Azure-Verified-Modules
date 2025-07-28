49415a57baa78b66af06ec7484966e015260a3eb
Rainer Halanek <rahalan@microsoft.com>
2025-07-28 17:15:49 +0200

    feat: Fix artifact download condition in New-AzAdvertizerDiffIssue function

--- utilities/pipelines/platform/New-AzAdvertizerDiffIssue.ps1
+++ utilities/pipelines/platform/New-AzAdvertizerDiffIssue.ps1
@@ -54,7 +54,7 @@ function New-AzAdvertizerDiffIssue {
   # get artifact (AzAdvertizerData.csv) from the latest workflow run
   $artifact = gh run download $latestWorkflowRunId --repo $Repo --dir /old --name 'AzAdvertizerData.csv'
 
-  if ($artifact -ne 'no valid artifacts found to download') {
+  if ($null -ne $artifact) {
     Write-Host "Downloaded artifact: $artifact"
     $issuesCreated = 0
     $addedData = Get-AzAdvertizerDataDiff -Path '/old/AzAdvertizerData.csv'
